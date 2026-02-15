16bd2efeca0cf630dc1a255b3b07c03ad6ebda84
AleidBanan <Aleidbanan@gmail.com>
2026-02-15 16:36:56 +0000

    task 0-4

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""This module provides a function that adds two floats."""
 
 def add(a: float, b: float) -> float:
+    """Return the sum of two floats."""
     return a + b
--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""This module defines and annotates basic variables."""
 
 a: int = 1
 pi: float = 3.14
