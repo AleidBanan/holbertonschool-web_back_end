e1c5c2ed1134302b4e04ade88b87da78b0cc4ff9
AleidBanan <Aleidbanan@gmail.com>
2026-02-16 11:55:40 +0000

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module description."""
+
+from typing import List
+
+
+def function_name(parameter: List[type]) -> return_type:
+    """Function description."""
+    return something
