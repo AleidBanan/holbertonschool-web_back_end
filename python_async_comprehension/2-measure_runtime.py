c364dd5fb2a23e503297891742c0932010062d02
AleidBanan <Aleidbanan@gmail.com>
2026-02-19 08:35:02 +0000

    tasks

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
-"""Module that provides a measure_time function."""
+"""
+Module that imports function
+and measures the total execution time
+"""
+
 
 import asyncio
 import time
-
+from typing import Callable
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measures total runtime of wait_n and returns average time per call."""
-    start_time = time.time()
+    """Return average runtime of wait_n"""
+    start = time.time()
+
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start_time
+
+    end = time.time()
+
+    total_time = end - start
     return total_time / n
