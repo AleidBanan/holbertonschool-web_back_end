0922909d1cdbf46aabd81d1a24f90503f7c36986
AleidBanan <Aleidbanan@gmail.com>
2026-04-01 19:46:16 +0000

    0-promise.js

---
+++ ES6_promise/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'semi': ['error', 'always'],
+    'no-console': 'off',
+  },
+};
+
